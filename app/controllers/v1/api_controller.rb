a885aaa2caa2bb5fcb3d4e0ebb6fe13af5f1518d
samkpower <samanthakpower@gmail.com>
2016-10-06 21:50:17 -0400

    Allow cross origin requests

---
+++ app/controllers/v1/api_controller.rb
@@ -0,0 +1,18 @@
+class V1::ApiController < ApplicationController
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
+  respond_to :json
+
+  after_filter :set_headers
+
+  def preflight_request
+    render json: :ok
+  end
+
+  private
+
+  def set_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Content-Type'] = 'application/json'
+  end
+end
